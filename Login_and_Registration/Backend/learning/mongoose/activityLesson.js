e43700cf96aa450b1c3a41971dc2a835e444d99e
russelPogi <russeljeoff2@gmail.com>
2026-08-18 13:29:14 +0800

    nothing

--- Login_and_Registration/Backend/learning/mongoose/activityLesson.js
+++ Login_and_Registration/Backend/learning/mongoose/activityLesson.js
@@ -9,38 +9,6 @@ function activity(app, EmployeeModel){
         res.json(employ)
     })
 
-    //find method - get data out
-    app.get('/example1', async (req, res)=>{
-
-        //without filter - get everything
-        const noFilter = await EmployeeModel.find()
-
-        //with filter
-        const filter = await EmployeeModel.find({
-            //only get the collections with the department: IT
-            department: 'IT'
-        })
-
-        //findOne
-        const fOne = await EmployeeModel.findOne({
-            email: 'russel@example.com'
-        })
-
-        //greaterThan
-        const getGt30k = await EmployeeModel.find({
-            salary: { $gt: 30000}  //only returns bob and charlie
-        })
-
-        //lessThan
-        const getLt35k = await EmployeeModel.find({
-            salary: { $lt: 35000 } //only returns alice and russel
-        })
-
-
-        //output
-        res.json(noFilter)
-    })
-
 }
 
 module.exports = activity
