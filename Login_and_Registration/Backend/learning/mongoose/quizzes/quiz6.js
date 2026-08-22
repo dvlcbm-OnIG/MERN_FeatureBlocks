119753d64feb40b1434183e67a393f774a6ced7c
russel <russeljeoff2@gmail.com>
2026-08-22 15:37:52 +0800

    Refactor quiz6.js: add GET endpoint for retrieving all employees, enhance POST endpoint with new update operations, and handle employee not found response

--- Login_and_Registration/Backend/learning/mongoose/quizzes/quiz6.js
+++ Login_and_Registration/Backend/learning/mongoose/quizzes/quiz6.js
@@ -3,6 +3,13 @@ const DateTime = require('./DateTime')
 //Creating Documents
 function quiz6(app, emp){
 
+    app.get('/example1', async (req, res)=>{
+
+        const findAll = await emp.find({
+        })
+        res.json(findAll)
+    })
+
     app.post('/example1', async(req, res)=>{
         
         //item1
@@ -21,13 +28,15 @@ function quiz6(app, emp){
         // const item3 = await emp.findOneAndUpdate(
         //     {name: 'Bob'},
         //     {$inc: {salary: 5000}},
-        //     {new: true}
+        //     {new: true}  Return the document AFTER the update
         // )
 
         //item4
         const item4 = await emp.findByIdAndUpdate(
             '6a8821813a58fb792f5f5331',
-            {$set: {department: 'Finance'}}
+            {$set: {department: 'Finance'}},
+            { new: true }
+
         )
 
         //item5
@@ -37,14 +46,53 @@ function quiz6(app, emp){
         // )
 
         //item6
-        await emp.updateOne(
-            {name: "Russel"},
-            {$max: {salary: 30000}}
+        // await emp.updateOne(
+        //     {name: "Russel"},
+        //     {$max: {salary: 30000}}
         
-        )        
+        // )
+        
+
+        //item7
+        await emp.updateMany(
+            {department: 'HR'},
+            {$max: {salary: 40000}}
+        )
+
+        //item8
+        await emp.updateOne(
+            {name: 'Russel'},
+            {$unset: {password: ''}} //removes the field "password"
+        )
+
+        // //item9
+        // await emp.updateMany(
+        //     {salary: {$lt: 30000}},
+        //     {$inc: {salary: 2000}}
+        // )
+
+        //item10
+        const item10 = await emp.findOneAndUpdate(
+            {email: 'lol'},
+            {$mul: {salary: 1.10}},
+            {new: true}
+        )
+        
+        if(item10){
+            res.json(item10)
+        }else{
+            res.json("Employee not found")
+        }
 
 
-       res.json(item4) 
+
+        // //applied modified time tracker
+        // await emp.updateMany(
+        //     {modifiedTime: {$exists: false}},
+        //     {$set: {modifiedTime: new Date()}}
+        // )
+
+
     })
 }
 
