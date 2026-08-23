60c7757b5f04a2ab3d09fda7df426ac0c26ab6a9
russel <russeljeoff2@gmail.com>
2026-08-23 23:11:45 +0800

    Refactor login API endpoint and update response structure

--- Login_and_Registration/Backend/learning/testAPI/server.js
+++ Login_and_Registration/Backend/learning/testAPI/server.js
@@ -1,30 +1,42 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const fs = require('fs')
 
+//middleware
 app.use(express.json())
 
+//store data
 let users = []
-app.post('/users2', (req, res) => {
+
+
+//when submit data
+app.post('/loginApi', (req, res) => {
+
     users.push(req.body)
-    res.json(req.body)
+
+    res.json({
+        message: "Login data received"
+    });
 })
 
-//render html
-app.get('/users2', (req, res) => {
-    
+//query the sent api
+app.get('/loginApi', (req, res) => {
     res.json(users)
 })
-app.get('/users1', (req, res) => {
-    
+
+app.get('/fetch', (req, res) => {
     res.sendFile(path.join(__dirname, 'fetch.html'))
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.use((req, res) => {
     res.send('404 not found')
 })
 
-app.listen(3000, () => {
-    console.log('server running')
+const IPv4 = '192.168.68.113'
+app.listen(3000, IPv4, () => {
+    console.log(`server running on port 30000, or to any devices: http://${IPv4}:3000`)
 })
